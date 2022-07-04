64e203a42002bf5363bf2f5b85bc665b75adba4b
lixuexin03 <lixuexin03@meituan.com>
2022-07-04 20:59:19 +0800

    wip

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log('xx')
+const taskName = process.argv[2];
+if (!taskName) {
+  throw new Error(`empty taskName`);
+}
+const { cosmiconfig } = require('cosmiconfig');
+const explorer = cosmiconfig('nodemon');
+const path = require('path');
+explorer
+  .search(path.join(__dirname, `./tasks/${taskName}`))
+  .then((result) => {
+    console.log('success', result);
+    const nodemon = require('nodemon');
+    nodemon({
+      ...result.config
+    });
+
+    nodemon
+      .on('start', function () {
+        console.log('App has started');
+      })
+      .on('quit', function () {
+        console.log('App has quit');
+        process.exit();
+      })
+      .on('restart', function (files) {
+        console.log('App restarted due to: ', files);
+      });
+  })
+  .catch((error) => {
+    console.error('error', error);
+  });
